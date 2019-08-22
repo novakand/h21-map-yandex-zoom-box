cecd2769683440b791bd8e1a659be3a466d1cbcc
novakand <45287302+novakand@users.noreply.github.com>
2019-08-22 17:31:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+{
+  "name": "@h21-map/yandex-zoom-box",
+  "scope":"@h21-map",
+  "version": "1.0.1",
+  "description": "A library of Yandex Map JS API",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/novakand/h21-map-yandex-zoom-box"
+  },
+  "keywords": [
+    "yandex",
+    "yandex-map",
+    "zoom-box",
+    "angular"
+  ],
+  "author": "",
+  "license": ""
+}
+
